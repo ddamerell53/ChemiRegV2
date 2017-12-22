36aa7ebdf3e1a7221d13f0300002c24c6078c3e5
David Damerell <david.damerell@sgc.ox.ac.uk>
2017-12-22 13:14:40 +0000

    Added docker files

---
+++ docker/build.ps1
@@ -0,0 +1,11 @@
+﻿docker pull postgres:9.6.6
+cd sgc_rdkit_postgres
+wget https://github.com/rdkit/rdkit/archive/Release_2017_09_1.tar.gz
+docker build -t sgc_rdkit_postgres:latest .
+cd ../sgc_rdkit_postgres_bigm
+docker build -t sgc_rdkit_postgres_bigm:latest .
+cd ../chemireg_postgres
+docker build -t chemireg_postgres:latest .
+cd ../chemireg
+docker pull continuumio/anaconda3
+docker build -t chemireg .
---
+++ docker/clean.ps1
@@ -0,0 +1,2 @@
+﻿docker rm $(docker ps -a -q)
+docker rmi $(docker images -a -q)
