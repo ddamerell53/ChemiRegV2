afa0346b4a708a1b2a8031b654486a8dde6783c9
David Damerell <david.damerell@sgc.ox.ac.uk>
2018-11-29 12:32:24 +0000

    [Start] Removed all absolute paths

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd /home/chemireg
-redis-4.0.6/src/redis-server --port 6380&
+redis-server --port 6380&
 export DEBUG=*
-cd src
 node chemireg.js services/chemireg.json&
-/opt/conda/bin/jupyter notebook --notebook-dir=/home/chemireg/src --ip='*' --port=8888 --no-browser
+jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser
