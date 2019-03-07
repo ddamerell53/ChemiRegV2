9f67368910fa0b4a8d64de5bc73b436ed603e06b
David Damerell <david.damerell@sgc.ox.ac.uk>
2019-03-07 13:00:23 +0000

    [Docker] Altered start-up script to install base custom fields, projects, and users

--- src/start.sh
+++ src/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 redis-server --port 6380&
 export DEBUG=*
+
+python install_base_db.py
+
 node chemireg.js services/chemireg.json&
 jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser
