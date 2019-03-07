95d807f793fba7a39801e6a2d53b448fbb590eea
David Damerell <david.damerell@sgc.ox.ac.uk>
2019-03-07 20:32:05 +0000

    [Travis] Added new further cut-down Travis build

---
+++ docker/build_travis_minimal.sh
@@ -0,0 +1,12 @@
+﻿docker pull postgres:9.6.6
+cd sgc_rdkit_postgres
+docker pull sgcit/chemireg:sgc_rdkit_postgres_bigm
+cd ../chemireg_postgres
+docker build -t chemireg_postgres:latest . 
+cd ../../
+docker build --build-arg VERSION=`date +%s` -t chemireg -f docker/chemireg_code_update/Dockerfile .
+
+docker tag chemireg sgcit/chemireg:latest
+docker tag chemireg_postgres sgcit/chemireg:postgres
+
+
