ff5a725e4254b590e5b9243c2a3f4fe39d3b3422
David Damerell <david.damerell@sgc.ox.ac.uk>
2019-03-08 10:59:32 +0000

    [REST API] Extracted REST API redirects into separate JS file

--- src/start.sh
+++ src/start.sh
@@ -6,7 +6,7 @@ sleep 5
 
 python install_base_db.py
 
-node chemireg.js services/chemireg.json&
+node start.js services/chemireg.json&
 jupyter notebook --notebook-dir=. --ip='0.0.0.0' --port=8888 --no-browser
 
 
