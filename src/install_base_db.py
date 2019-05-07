c7ab5cc5d7bead0e3007f8448216807a716d98ec
David Damerell <david.damerell@sgc.ox.ac.uk>
2019-05-07 15:11:03 +0100

    [Docker] Added calls to pull projects created using legacy commands into the main custom entity environment

--- src/install_base_db.py
+++ src/install_base_db.py
@@ -2,6 +2,7 @@ import os
 
 if not os.path.exists('db_created'):
     from authenticate import AuthenticationManager
+    from patcher import Patcher
 
     manager = AuthenticationManager()
     manager.add_user_projects('administrator')
@@ -9,6 +10,15 @@ if not os.path.exists('db_created'):
 
     manager.create_test_environment(s_p)
     manager.create_wordpress_environment(s_p)
+
+    patcher = Patcher()
+
+    patcher.import_projects2()
+    patcher.import_users_from_user_table()
+    patcher.import_custom_fields()
+    patcher.import_user_to_project()
+
+    patcher.manager.commit()
 
     with open('db_created', 'w') as fw:
         fw.write('Base DB Created\n')
